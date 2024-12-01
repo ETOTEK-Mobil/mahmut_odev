050e35f3d9efa87ad5f92d815d1e4e2fb4d2836c
Mahmut Karadeniz <mahmutkaradeniz13@gmail.com>
2024-12-01 03:17:37 +0300

    Week 2 Ödev

--- lib/main.dart
+++ lib/main.dart
@@ -1,49 +1,19 @@
 import 'package:flutter/material.dart';
-import 'package:odev/MainPage.dart';
-void main() {
-  runApp(const MyApp());
+import 'package:odev/core/init/app_init.dart';
+import 'package:odev/ui/view/home/home_view.dart';
+
+void main() async {
+  await AppInit.init();
+  runApp(const MainApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class MainApp extends StatelessWidget {
+  const MainApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const MainPage(),
-
+    return const MaterialApp(
+      home: HomeView(),
     );
   }
 }
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(widget.title),
-      ),
-      body: const Center(
-        child: Column(
-        ),
-      ),
-    );
-  }
-}
-
-
--- lib/ui/view/hafta_2/hafta_2_view.dart
+++ lib/ui/view/hafta_2/hafta_2_view.dart
@@ -1,10 +1,48 @@
 import 'package:flutter/material.dart';
+import 'package:odev/ui/view/hafta_2/pages/buttons_page.dart';
+import 'package:odev/ui/view/hafta_2/pages/images_page.dart';
+import 'package:odev/ui/view/hafta_2/pages/picker_page.dart';
+import 'package:odev/ui/view/hafta_2/pages/progress_sliderBar_page.dart';
+import 'package:odev/ui/view/hafta_2/pages/text_textField_page.dart';
+import 'package:odev/ui/view/hafta_2/pages/web_view_page.dart';
+import 'package:odev/ui/widgets/buttons/common_button.dart';
 
 class Hafta2View extends StatelessWidget {
   const Hafta2View({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    final _pageNames = [
+      'Text & TextField',
+      'Butonlar',
+      'Progress Bar & Slider',
+      'Webview',
+      'Resimler',
+      'Dropdown Button & Time Date Picker',
+    ];
+    final _pages = [
+      TextTextFieldPage(),
+      const ButtonsPage(),
+      const ProgressSliderBarPage(),
+      const WebViewPage(),
+      const ImagesPage(),
+      const PickerPage(),
+    ];
+    //CommonButton(page: _pages[0], pageName: _pageNames[0]);
+    return Scaffold(
+      body: Center(
+        child: ListView.separated(
+            shrinkWrap: true,
+            itemBuilder: (context, index) => Align(
+                  alignment: Alignment.center,
+                  child: CommonButton(
+                    page: _pages[index],
+                    pageName: _pageNames[index],
+                  ),
+                ),
+            separatorBuilder: (context, index) => const SizedBox(height: 20),
+            itemCount: _pages.length),
+      ),
+    );
   }
 }
